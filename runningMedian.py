e57de36a6323e9d643f1d1e3643abf19218c8fbd
6peterlu <6peterlu@gmail.com>
2017-08-15 15:00:50 -0700

    Create runningMedian.py

---
+++ runningMedian.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import sys
+
+class Heap:
+    def __init__(self, isMinHeap):
+        self.data = []
+        self.isMinHeap = isMinHeap
+    def insert(self, num):
+    def remove(self, num):
+    def upheap(self):
+    def downheap(self):
+    
+
+n = int(input().strip())
+a = []
+a_i = 0
+for a_i in range(n):
+   a_t = int(input().strip())
+   a.append(a_t)
